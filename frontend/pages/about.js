ec6b1fdf2235d0f57a039a004a3a3d8fa2c8d174
SamGodfrey1 <samgodfrey00@gmail.com>
2020-06-12 03:32:36 +0100

    added about page

---
+++ frontend/pages/about.js
@@ -0,0 +1,13 @@
+import Layout from '@/components/layout'
+
+import Link from 'next/link'
+import Router from 'next/router'
+
+export default function About() {
+    return (
+        <Layout title="About - Billpop">
+        <h1>Sam</h1>
+
+        </Layout>
+    )
+}
