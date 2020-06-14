c2c225de5525cac13c79491ffa82071addf66ff2
SamGodfrey1 <samgodfrey00@gmail.com>
2020-06-14 02:51:27 +0100

    added about page

---
+++ frontend/pages/selling.js
@@ -0,0 +1,20 @@
+import Layout from '@/components/layout'
+
+import Link from 'next/link'
+import Router from 'next/router'
+
+export default function Selling() {
+    return (
+        <Layout title="Selling - Billpop">
+            <div
+                id="hero"
+                className="min-w-max-content bg-cover bg-center overflow-x-hidden"
+                style={{
+                    backgroundColor: '#7786D7',
+                    backgroundImage: `url('http://localhost:3000/selling-img2.jpg')`,
+                    height: '477px',
+                }}
+            ></div>
+        </Layout>
+    )
+}
