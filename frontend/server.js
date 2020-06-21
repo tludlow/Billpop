7f5e804ed3c89c1b9735d9db3059a2a69d521205
BillyElderfield <billyelderfield1@hotmail.com>
2020-06-21 23:27:05 +0100

    Fixed build error

--- frontend/server.js
+++ frontend/server.js
@@ -2,16 +2,11 @@ const { createServer } = require('https')
 const { parse } = require('url')
 const next = require('next')
 const fs = require('fs')
-const express = require('express')
 var http = require('http')
 
-console.log('test: ' + fs.existsSync('/ssl/'))
-console.log('key: ' + fs.existsSync('/ssl/privkey.pem'))
-console.log('cert: ' + fs.existsSync('/ssl/cert.pem'))
 const dev = fs.existsSync('./ssl')
 console.log('dev mode: ' + dev)
 
-const port = 3000
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -39,13 +34,3 @@ app.prepare().then(() => {
         console.log('> Ready on https://localhost:3000')
     })
 })
-
-// app.use(function (req, res, next) {
-//     if (req.secure) {
-//         // request was via https, so do no special handling
-//         next()
-//     } else {
-//         // request was via http, so redirect to https
-//         res.redirect('https://' + req.headers.host + req.url)
-//     }
-// })
